a28896463114a69098cf917565a361073b9ea6c5
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2023-11-03 01:57:28 +0530

    m

--- fitness-web/src/Components/footer.js
+++ fitness-web/src/Components/footer.js
@@ -1,35 +1,41 @@
-// src/comopnents/Footer.js
-
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-
-        <div className="bg-[#FA7D19] text-white">
-            <footer className="footer p-10 justify-items-center">
-                <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Developing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </div>
-                <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </div>
-                <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+        <div className="bg-footer">
+            <footer className="footer p-10 text-center">
+                <div className="footer-row">
+                    <div id='Se' className='tag'>
+                        <div>
+                            <span className="footer-title">Services</span>
+                            <a className="link link-hover">Branding</a>
+                            <a className="link link-hover">Design</a>
+                            <a className="link link-hover">Developing</a>
+                            <a className="link link-hover">Advertisement</a>
+                        </div>
+                    </div>
+                    <div className='tag'>
+                        <div>
+                            <span className="footer-title">Company</span>
+                            <a className="link link-hover">About us</a>
+                            <a className="link link-hover">Contact</a>
+                            <a className="link link-hover">Jobs</a>
+                            <a className="link link-hover">Press kit</a>
+                        </div>
+                    </div>
+                    <div id='Le' className='tag'>
+                        <div>
+                            <span className="footer-title">Legal</span>
+                            <a className="link link-hover">Terms of use</a>
+                            <a className="link link-hover">Privacy policy</a>
+                            <a className="link link-hover">Cookie policy</a>
+                        </div>
+                    </div>
                 </div>
             </footer>
-            <div className="text-center py-8">
-                <p>Copyright © 2022 - All right reserved by Codebun</p>
+            <div id='bot' className="text-center" >
+                <p>Copyright © 2023 - All rights reserved by Fitness Web</p>
             </div>
         </div>
     );
--- fitness-web/src/Components/homePage.js
+++ fitness-web/src/Components/homePage.js
@@ -6,10 +6,20 @@ const HomePage = () => {
     <div>
 
 <div className="banner-container">
-                <div className="text-center">
-                    <h1 className=" text-6xl text-orange-600 font-bold">Welcome to Fitness Web</h1>
-                    <h4 className="text-4xl mt-8 text-white"> Your very own personal tutorial corner</h4>
-                </div>
+                {/* <div className="text-center">
+                    <h1 id='tx1' className=" text-6xl ">Welcome to Fitness Web</h1>
+                    <h4 id='tx2' className="text-4xl "> Your very own personal tutorial corner</h4>
+                </div> */}
+                 <div class="container">
+        <div class="centered-container">
+            <div className='col'>
+              <h4>this is for add report</h4>
+            </div>
+        </div>
+        <div class="centered-container">
+           
+        </div>
+    </div>
             </div>
     </div>
   )
