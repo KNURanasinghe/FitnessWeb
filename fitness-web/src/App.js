b970146ee6fe6f19a6743d43b3ace196f9ab3821
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2023-11-05 03:48:02 +0530

    1

--- fitness-web/src/App.js
+++ fitness-web/src/App.js
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-// import Home from './Components/homePage';
-// import About from './Components/about';
-// import Profile from './Components/profile';
+import Home from './Components/homePage';
+import About from './Components/about';
+import Profile from './Components/profile';
 
-// function App() {
-//   return (
-//     <Router>
+function App() {
+  return (
+    <Router>
       
-//         <Route path="/" exact component={Home} />
-//         <Route path="/about" component={About} />
-//         <Route path="/profile" component={Profile} />
-//         <Route path="/" component={Profile} />
+        <Route path="/homePage" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profile" component={Profile} />
+        
       
-//     </Router>
-//   );
-// }
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
