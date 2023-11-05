007e1e2371c2f5e011256fdc7b74fe91d77f38b3
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2023-11-05 13:33:56 +0530

    54

--- fitness-web/src/index.js
+++ fitness-web/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import SignUp from './Components/signUp';
+import Login from './Components/login';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <SignUp />
+      <Login />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
